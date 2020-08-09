5ef0f23d452937ca302b581783f51d1f3328e741
Alex Zaslavskis <sahsariga111@gmail.com>
2020-08-09 14:27:02 +0300

    Update Form1.cs

--- APP/WindowsFormsApp1/Form1.cs
+++ APP/WindowsFormsApp1/Form1.cs
@@ -19,15 +19,8 @@ namespace WindowsFormsApp1
     {
         public Form1()
         {
-
             InitializeComponent();
             this.Text = "CoronaVirus Simple Numbers";
-            /*
-             * String[] data=title.split(" ");
-        int cases=Integer.parseInt(data[3].replace(",",""));
-        int deaths=Integer.parseInt(data[6].replace(",",""));
-        */
-
             var webGet = new HtmlWeb();
             var document = webGet.Load("https://www.worldometers.info/coronavirus/");
             var title = document.DocumentNode.SelectSingleNode("html/head/title").InnerText;
@@ -44,44 +37,23 @@ namespace WindowsFormsApp1
             var path = @"./tmp.txt";
             if (File.Exists(path))
             {
-
-
                 string[] file = File.ReadAllLines(path);
-
                 new SaveData().save_data(data[6].Replace(',', ' '), data[3].Replace(',', ' '));
-
             }
-
         }
         void notification()
         {
-           
             var webGet = new HtmlWeb();
             var document = webGet.Load("https://www.worldometers.info/coronavirus/");
             var title = document.DocumentNode.SelectSingleNode("html/head/title").InnerText;
             var notification = new System.Windows.Forms.NotifyIcon()
             {
-
                 Visible = true,
                 Icon = System.Drawing.SystemIcons.Warning,
-                // optional - BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info,
-                // optional - BalloonTipTitle = "My Title",
                 BalloonTipText = title,
             };
-
-            // Display for 5 seconds.
             notification.ShowBalloonTip(5000);
-
-            // This will let the balloon close after it's 5 second timeout
-            // for demonstration purposes. Comment this out to see what happens
-            // when dispose is called while a balloon is still visible.
-
-            // The notification should be disposed when you don't need it anymore,
-            // but doing so will immediately close the balloon if it's visible.
-            notification.Dispose();
-   
-        
-    }
-      
+            notification.Dispose();        
+    }   
     }
 }
